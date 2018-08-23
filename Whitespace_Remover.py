18ec9da2e49cf749c076eab2dc9eb005b3bfc772
echu87 <echu1@ocdsb.ca>
2018-08-22 20:40:51 -0400

    added Whitespace_Remover class, which currently detects the fully white rows in the picture

--- Whitespace_Remover.py
+++ Whitespace_Remover.py
@@ -6,10 +6,23 @@ im = Image.open(basepath + "\\" + "ConvertedImages\\" + "Frontier-0.png", 'r')
 width, height = im.size
 pixel_values = list(im.getdata())
 pix = im.load()
-count = 0
-for x in range (width):
-    for y in range(height):
-        count +=1
-        print(pix[x,y])
-print (count)
 
+white_row = 0
+white_pixel = 0
+rows_to_be_removed = []
+
+#iterate over all the pixels in the picture, if the white count of a row is greater or equal to the width (the entire row is white), add the y value (row), to the rows_to_be_removed list.
+for y in range (height):
+    for x in range(width):
+        if white_pixel >=width:
+            rows_to_be_removed.append(y)
+            white_pixel = 0
+        elif pix[x,y] == 0:
+            white_pixel +=1
+
+print (rows_to_be_removed)
+
+
+
+
+#if white_pixel exceeds width, increase white row. If white row exceeds 100, delete the 100 rows.
