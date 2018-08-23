dcc67b94ef8b71c73f8e60fc7124519616846b63
Youssef Zayed <yzaye1@ocdsb.ca>
2018-08-23 19:03:14 -0400

    Testing Kivy further

--- Kivy/kivy test HelloWorld/Gui.py
+++ Kivy/kivy test HelloWorld/Gui.py
@@ -1,16 +1,24 @@
 from kivy.app import App
 from kivy.uix.button import  Button
+from kivy.uix.scatter import Scatter
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
 
-
-class HelloWorld(App):
+class ScatterTextWidget(BoxLayout):
+    pass
+class hello_world(App):
     def build(self):
         btn = Button(text= "Push Me!")
-        return btn
+        def callback(instance):
+            print("Hellon't")
+        btn.bind(on_press=callback)
+        return ScatterTextWidget()
 
-class World(App):
-    def build(self):
-        return HelloWorld()
+
+
 
 
 if __name__ == '__main__':
-    World().run()
+    hello_world().run()
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
+Requirements:
+
 This project requires Kivy for gui inegration. 
 install kivy from https://kivy.org/docs/installation/installation-windows.html
 for best result use python 3.5 or higher and install kivy.deps.angle
-
-Requirements:
 
 pip install AWSCLI
 pip install BOTO3
